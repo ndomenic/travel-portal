a43333bf311172889fb37e4ec1c94a72fd165c33
Nicholas Domenichini <nicholician@gmail.com>
2019-04-08 20:30:04 -0400

    Removed unnecessary gitignore

---
+++ client/build/precache-manifest.64e3dd28e07b199890c78be861822482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5827db3e31fbc63990de",
+    "url": "/static/js/main.17f2040f.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5827db3e31fbc63990de",
+    "url": "/static/css/main.4087852c.chunk.css"
+  },
+  {
+    "revision": "a4ddecc2a006d2845e89292da56629ad",
+    "url": "/index.html"
+  }
+];
