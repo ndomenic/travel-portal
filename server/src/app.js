3534103a9c6f84ad50de50de72266c73e9a050dd
Nicholas Domenichini <nicholician@gmail.com>
2019-04-08 20:20:19 -0400

    Fixed up the server docker and source files

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 
 const buildDir = __dirname.substring(0, __dirname.length - 10) + 'client/build'
 
 const app = express();
+
+// Use cors options to availbe cross-origin errors
+app.use(cors());
 
 // Serve the static files from the React app
 app.use(express.static(buildDir));
@@ -11,7 +15,7 @@ app.use(express.static(buildDir));
 // An api endpoint that returns a short list of items
 app.get('/helloWorld', (req,res) => {
 	console.log("test");
-    res.json({"foo": "test2"});
+    res.json({"foo": "bar"});
 });
 
 // Handles any requests that don't match the ones above
